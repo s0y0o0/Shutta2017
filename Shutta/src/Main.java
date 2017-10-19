5de82c8228d1bb62cd9580269aa099f087bb1362
SEY <s0y0o01211@gmail.com>
2017-10-19 15:13:12 +0900

    //

--- Shutta/src/Main.java
+++ Shutta/src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
 
+        Player p1 = new Player();
+        Player p2 = new Player();
+
+        //while
+        Round round = new Round();
+
+        Dealer.getInstance().pickCards(p1, p2);
+
+        int score1 = p1.getCardPair().calculateScore();
+        int score2 = p2.getCardPair().calculateScore();
+
+        round.setWinner(score1, score2);
+
+
+
+        //Game.getInstance().add(round);
+
+
+        /*
+        Card c1 = new Card();
+        c1.setGwang(true);
+        c1.setNum(10);
+
+        Card c2 = new Card();
+        c2.setGwang(false);
+        c2.setNum(1);
+
+        CardPair cp= new OriginalCardPair();
+
+        */
     }
 }
