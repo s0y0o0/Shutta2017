0893b84504a9bd3f0defd775ffb6608b0f3d880f
SEY <s0y0o01211@gmail.com>
2017-10-19 14:36:23 +0900

    Merge branch 'master' of https://github.com/s0y0o0/Shutta2017
    # Conflicts:
    #	Shutta/src/CardPair.java

--- Shutta/src/CardPair.java
+++ Shutta/src/CardPair.java
@@ -1,15 +1,17 @@
 abstract public class CardPair {
+    static boolean tie = false;
     private Card[] _cardPair;
 
     public Card[] getCardPair() {
         return _cardPair;
     }
-
     public void setCardPair(Card[] cardPair) {
         _cardPair = cardPair;
     }
 
+    public static void main(String[] args){
 
+    }
 }
 
 
