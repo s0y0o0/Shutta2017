6b68c1a2ae69ce39878e7b8bba87abe82bda8c6a
kcci02 <jeongyoon125@gmail.com>
2017-10-19 12:03:58 +0900

    class defined

--- Shutta/src/CardPair.java
+++ Shutta/src/CardPair.java
@@ -1,41 +1,4 @@
 public class CardPair {
-    //region Card[] _cardPair
-    private Card[] _cardPair;
 
-    public Card[] getCardPair() {
-        return _cardPair;
-    }
-
-    public void setCardPair(Card[] cardPair) {
-        _cardPair = cardPair;
-    }
-    //endregion
-
-    class Card {
-        //region int _num
-        private int _num;
-
-        public int getNum() {
-            return _num;
-        }
-
-        public void setNum(int num) {
-            _num = num;
-        }
-        //endregion
-
-        //region boolean _gwang
-        private boolean _gwang;
-
-        public boolean getGwang() {
-            return _gwang;
-        }
-
-        public void setGwang(boolean gwang) {
-            _gwang = gwang;
-        }
-        //endregion
-
-    }
-
+    private int[] cards;
 }
